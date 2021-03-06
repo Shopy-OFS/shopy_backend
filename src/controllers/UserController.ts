e6ea0d764f274cf029402eeb77d5de96904d5af0
shopy <uni.group199x@gmail.com>
2021-03-07 00:34:13 +0700

    add more method

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -7,7 +7,12 @@ const userController = {
     res.send(data);
   },
   show: async (req: Request, res: Response) => {
-    res.send(res);
+    const data = await User.findByPk(req.params.id)
+    if(data){
+     res.send(data);
+      return;
+    }
+    res.status(400).send({message: 'Not found'});
   },
   update: async (req: Request, res: Response) => {},
   destroy: async (req: Request, res: Response) => {},
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,5 +4,6 @@ import userController from '../controllers/UserController';
 const userRoutes = Router();
 
 userRoutes.get('/users', userController.index);
+userRoutes.get('/users/:id', userController.show);
 
 export default userRoutes;
