59e72b407011e241af0843435739bf41a5715a56
Ba Huy <thainguyenbahuy@gmail.com>
2021-04-07 23:41:49 +0700

    create response helper

---
+++ src/helpers/ResponseApi.ts
@@ -0,0 +1,18 @@
+import { Response } from 'express';
+
+const sendSuccess = (response: Response, message: string | null, data: any) =>
+  // eslint-disable-next-line implicit-arrow-linebreak
+  response.send({
+    message,
+    ...data,
+  });
+
+const sendError = (response: Response, httpCode: number, message: string | null, error: any) =>
+  // eslint-disable-next-line implicit-arrow-linebreak
+  response.status(httpCode).send({
+    message,
+    ...error,
+  });
+
+// eslint-disable-next-line import/prefer-default-export
+export { sendSuccess, sendError };
