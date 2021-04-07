cbed76191921c46acc655677fefc50b68d30b320
Ba Huy <thainguyenbahuy@gmail.com>
2021-04-08 00:01:33 +0700

    create migration admin_users

---
+++ src/sqlz/models/AdminUser.model.ts
@@ -0,0 +1,36 @@
+import { Table, Column, Model, DataType, DefaultScope } from 'sequelize-typescript';
+
+@DefaultScope(() => ({
+  attributes: {
+    exclude: ['password', 'createdAt', 'updatedAt', 'deletedAt'],
+  },
+}))
+@Table({
+  tableName: 'admin_users',
+  timestamps: true,
+})
+export default class AdminUser extends Model {
+  @Column({
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id!: number;
+
+  @Column(DataType.STRING)
+  full_name?: string;
+
+  @Column(DataType.STRING)
+  email?: string;
+
+  @Column(DataType.STRING)
+  password?: string;
+
+  @Column(DataType.DATE)
+  createdAt?: Date;
+
+  @Column(DataType.DATE)
+  updatedAt?: Date;
+
+  @Column(DataType.DATE)
+  deletedAt?: Date;
+}
